5fa9992988d717a93cf9095a2f9d77d9b7be62fe
appiahkubis14 <appiahkubis14@gmail.com>
2025-03-22 19:33:42 +0000

    first commit

---
+++ ampPortal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AmpportalConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ampPortal"
---
+++ ampPortal/middleware/__init__.py
@@ -0,0 +1,2 @@
+from .loginrequired_middleware import LoginRequiredMiddleware  # noqa: F401
+from .sidebar_middleware import SidebarMiddleware # noqa: F401
